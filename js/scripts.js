db44ba4a8a4ec8244d62425bb249944adadf4d2e
David Abel & Courtney Phillips <student@epicodus.com>
2015-06-02 16:27:54 -0700

    Adds all functions and initial styling to the page.

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+function MovieTicket(movieName, showing, age, cost){
+  this.movieName = movieName
+  this.showing = showing
+  this.age = age
+  this.cost = 10;
+}
+
+MovieTicket.prototype.price = function() {
+
+  if (this.showing === "matinee") {
+    this.cost -= 3;
+  }
+
+  if (this.age === "senior" || "child") {
+    this.cost -= 2;
+  }
+
+  return this.cost;
+}
+
+$(document).ready(function(){
+
+  $("form#purchase").submit(function(event){
+    event.preventDefault();
+    var movie = $("input[name=movie]:checked").val();
+    var showing = $("input[name=showtime]:checked").val();
+    var type = $("input[name=type]:checked").val();
+
+    var newTicket = new MovieTicket(movie, showing, type)
+    newTicket.cost = newTicket.price();
+
+    $(".name").text(newTicket.movieName);
+    $(".showing").text(newTicket.showing);
+    $(".type").text(newTicket.age);
+    $(".cost").text(newTicket.cost);
+    $("#show-ticket").show();
+    $("div.form").hide();
+    $("button").hide();
+
+  });
+
+})
